ff1ff7e6131c951600606ba92582f43f40e24ddc
LvemiX520 <2904723491@qq.com>
2023-12-24 23:11:23 +0800

    Site updated: 2023-12-24 23:11:18

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,21 @@
-WIDGET={CONFIG:{modules:"01234",background:"5",tmpColor:"4A86E8",tmpSize:"16",cityColor:"FF9900",citySize:"16",aqiColor:"C97C08",aqiSize:"16",weatherIconSize:"24",alertIconSize:"18",padding:"10px 10px 6px 10px",shadow:"0",language:"auto",fixed:"false",vertical:"top",horizontal:"left",key:"ad1b5dfb806a42458fe46481c146a5f6"}};
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "5",
+        "tmpColor": "4A86E8",
+        "tmpSize": "16",
+        "cityColor": "FF9900",
+        "citySize": "16",
+        "aqiColor": "C97C08",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 6px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "fixed": "false",
+        "vertical": "top",
+        "horizontal": "left",
+        "key": "ad1b5dfb806a42458fe46481c146a5f6"
+    }
+}
